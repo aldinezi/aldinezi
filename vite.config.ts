086dd63c01790b4d9e9c7f14d42b824ea32960d1
Aldin Nezirić <aldinezi@gmail.com>
2025-04-21 15:30:08 +0200

    Update Vite configuration and index.html for improved asset handling
    - Set assetsDir in Vite config to organize output assets.
    - Enabled emptyOutDir to clear previous build files.
    - Updated index.html to reference assets with the correct base path.

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/aldinezi/", // Replace with your repository name
+  base: "/aldinezi/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
   build: {
     outDir: "docs",
+    assetsDir: "assets",
+    emptyOutDir: true,
   },
 });
